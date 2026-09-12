a24db79a65bbd77a733feae6bedc99a48963a636
kar cua <Tucorreo>
2026-09-11 23:10:58 -0600

    primer get

--- ISD/Primer parcial/01cliente.js
+++ ISD/Primer parcial/01cliente.js
@@ -1,29 +1,31 @@
 /*
-Vamos a programar un cliente, mediante 
-el cual vamos a probar 
+Vamos a programar un cliente mediante el cual vamos a probar la arquitectura cliente-servidor por medio de una petición mediante el protocolo HTTP,
+utilizando el método GET a la ruta: /api/talleres
 
-que tenemos que conectarnos al servicio de la base de datos "BD"
+Quw tenemos que conectarnos al servicio de la BD
 
-el problema es como apuntar al backend
+El problema es como apuntar al backend
+
 */
 
-const API_URL = "https://proyectobackcecyt9.onrender.com"
+const API_URL = "https://proyectobackcecyt9.onrender.com"; //Cambia esto segùn la URL de tu backend
 
 async function main() {
-
-    console.log("cliente Peticion Get -> a${API_URL}/api/talleres");
-    const inicio = Date.now();
-    const respuesta = await fetch('${API_URL}/api/talleres');
     
-    // nos da el tiempo de ida y vuelta por medio de la peticion mediante RTT
-    const duracionMS = Date.now() - inicio;
-    // un estatus de error 500 seria una respuesta valida si el servicico de la BD no esta disponible
-    const cuerpo = await respuesta.json();
+    console.log("Cliente Peticiòn Get -> a ${API_URL}/api/talleres");
 
-    console.log ("Clico de peticion respuesta ");
-    console.log("Protocolo HTTPS sobre protocolo TCP : ${respuesta.url} ");
-    console.log("Status de recibido: ${respuesta.status}${respuesta.statusText}");
-    console.log("Tiempo de ida y vuelta (RTT) ${duracionMS} ms");
-    console.log(JSON.stringify(cuerpo,null,2));
+    const inicio = Date.now();
+    const respuesta = await fetch(`${API_URL}/api/talleres`);
+    //Nos da el tiempo de ida y vuelta por medio de la petición mediante RTT
+    const duracionMs = Date.now() - inicio;
+    //un estatus de error 500 seria una respuesta valida si el servicio de la BD no esta disponible
+    const cuerpo = await respuesta.json();
+    console.log("Ciclo de petición - respuesta");
+    console.log("Protocolo HTTPS sobre TCP : ${respuesta.url}");
+    console.log("Status de recibido: ${respuesta.status} ${respuesta.statusText}");
+    console.log("Tiempo de ida y vuelta (RTT): ${duracionMs} ms");
+    console.log(JSON.stringify(cuerpo, null, 2));
+
 }
+
 main();
